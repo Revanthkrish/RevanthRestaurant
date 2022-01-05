16d8adf4c4e6eb9c894cd61f551c36b945472353
Revanthkrish <91882176+Revanthkrish@users.noreply.github.com>
2022-01-05 12:20:49 +0530

    Add files via upload

---
+++ Revanth.Web/SD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revanth.Web
+{
+    public static class SD
+    {
+        public static string ProductAPIBase { get; set; }
+
+        public static string ShoppingCartAPIBase { get; set; }
+
+        public static string CouponAPIBase { get; set; }
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
