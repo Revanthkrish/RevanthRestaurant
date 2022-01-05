a23dfad2c51e424085c03a00d14da90a27c89b21
Revanthkrish <91882176+Revanthkrish@users.noreply.github.com>
2022-01-05 12:35:57 +0530

    Add files via upload

---
+++ Revanth.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using Revanth.Services.ProductAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revanth.Services.ProductAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>();
+                config.CreateMap<Product, ProductDto>();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
