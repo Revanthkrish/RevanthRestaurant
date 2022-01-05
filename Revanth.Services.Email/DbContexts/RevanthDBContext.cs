d7a8ff49b57fa7e1d7a05eecd4c90020a90027b5
Revanthkrish <91882176+Revanthkrish@users.noreply.github.com>
2022-01-05 11:23:10 +0530

    Add files via upload

---
+++ Revanth.Services.Email/DbContexts/RevanthDBContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Revanth.Services.Email.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revanth.Services.Email.DbContexts
+{
+    public class RevanthDBContext:DbContext
+    {
+        public RevanthDBContext(DbContextOptions<RevanthDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<EmailLog> EmailLogs { get; set; }
+
+    }
+}
