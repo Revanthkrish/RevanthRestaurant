3efd5b6a97fc7324f7f536653c4a1c68c5e9f315
Revanthkrish <91882176+Revanthkrish@users.noreply.github.com>
2022-01-05 12:34:51 +0530

    Add files via upload

---
+++ Revanth.Services.ShoppingCartAPI/MappingConfig.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using Revanth.Services.ShoppingCartAPI.Models;
+using Revanth.Services.ShoppingCartAPI.Models.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Revanth.Services.ShoppingCartAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                config.CreateMap<ProductDto, Product>().ReverseMap();
+                config.CreateMap<CartHeader, CartHeaderDto>().ReverseMap();
+                config.CreateMap<CartDetails, CartDetailsDto>().ReverseMap();
+                config.CreateMap<Cart, CartDto>().ReverseMap();
+            });
+
+            return mappingConfig;
+        }
+    }
+}
